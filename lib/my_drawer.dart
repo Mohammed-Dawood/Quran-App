7b7d6a98ada8107d0d0b3224e2586d30472341d2
Mohammed Dawood <m.dawood.780909@gmail.com>
2023-03-05 04:57:05 +0100

    edit design

--- lib/my_drawer.dart
+++ lib/my_drawer.dart
@@ -1,75 +1,111 @@
-import 'constant.dart';
 import 'package:flutter/material.dart';
-import 'package:quran_app/settings.dart';
-import 'package:share_plus/share_plus.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:quran_app/constant.dart';
 
-class MyDrawer extends StatelessWidget {
+class MyDrawer extends StatefulWidget {
   const MyDrawer({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<MyDrawer> createState() => _MyDrawerState();
+}
+
+class _MyDrawerState extends State<MyDrawer> {
+  @override
   Widget build(BuildContext context) {
     return Drawer(
+      backgroundColor: const Color.fromRGBO(254, 249, 205, 1),
       child: ListView(
         padding: EdgeInsets.zero,
         children: [
           DrawerHeader(
             decoration: const BoxDecoration(
-              color: Colors.white,
+              color: Color.fromRGBO(6, 87, 96, 1),
             ),
-            child: Column(
-              children: [
-                Image.asset(
-                  'assets/quran.png',
-                  height: 80,
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.contain,
+                  image: AssetImage('assets/icon.png'),
                 ),
-                const Text(
-                  'Al Quran',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ],
+              ),
             ),
           ),
-          ListTile(
-            leading: const Icon(
-              Icons.settings,
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Directionality(
+              textDirection: TextDirection.rtl,
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.format_size_outlined,
+                    size: 25,
+                    color: Color.fromRGBO(6, 87, 96, 1),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'حجم الخط',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Color.fromRGBO(6, 87, 96, 1),
+                      fontFamily: 'quran',
+                      shadows: [
+                        Shadow(
+                          offset: Offset(.5, .5),
+                          blurRadius: 1.0,
+                          color: Color.fromRGBO(6, 87, 96, 1),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-            title: const Text(
-              'Settings',
-            ),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const Settings()));
-            },
           ),
-          ListTile(
-            leading: const Icon(
-              Icons.share,
-            ),
-            title: const Text(
-              'Share',
-            ),
-            onTap: () {
-              Share.share('''*Quran app*\n
-u can develop it from my github github.com/itsherifahmed ''');
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: const Icon(
-              Icons.rate_review,
-            ),
-            title: const Text(
-              'Rate',
-            ),
-            onTap: () async {
-              if (!await launchUrl(quranAppurl,
-                  mode: LaunchMode.externalApplication)) {
-                throw 'Could not launch $quranAppurl';
-              }
+          // Directionality(
+          //   textDirection: TextDirection.rtl,
+          //   child: ListTile(
+          //     leading: const Icon(
+          //       Icons.format_size_outlined,
+          //       size: 25,
+          //       color: Color.fromRGBO(6, 87, 96, 1),
+          //     ),
+          //     title: const Text(
+          //       'حجم الخط',
+          //       style: TextStyle(
+          //         fontSize: 20,
+          //         color: Color.fromRGBO(6, 87, 96, 1),
+          //         fontFamily: 'quran',
+          //         shadows: [
+          //           Shadow(
+          //             offset: Offset(.5, .5),
+          //             blurRadius: 1.0,
+          //             color: Color.fromRGBO(6, 87, 96, 1),
+          //           )
+          //         ],
+          //       ),
+          //     ),
+          //     onTap: () {
+          //       Navigator.pop(context);
+          //       Navigator.push(context,
+          //           MaterialPageRoute(builder: (context) => const Settings()));
+          //     },
+          //   ),
+          // ),
+          Slider(
+            activeColor: const Color.fromRGBO(6, 87, 96, 1),
+            value: arabicFontSize,
+            min: 20,
+            max: 40,
+            onChanged: (value) {
+              setState(
+                () {
+                  arabicFontSize = value;
+                },
+              );
+              saveSettings();
             },
           ),
         ],
--- lib/settings.dart
+++ lib/settings.dart
@@ -14,91 +14,125 @@ class _SettingsState extends State<Settings> {
     return Material(
       child: Scaffold(
         appBar: AppBar(
+          centerTitle: true,
           title: const Text(
-            "Settings",
+            'الإعدادات',
+            style: TextStyle(
+              fontFamily: 'quran',
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(254, 249, 205, 1),
+              shadows: [
+                Shadow(
+                  offset: Offset(.5, .5),
+                  blurRadius: 1.0,
+                  color: Color.fromRGBO(6, 87, 96, 1),
+                )
+              ],
+            ),
           ),
-          backgroundColor: const Color.fromARGB(255, 56, 115, 59),
+          backgroundColor: const Color.fromRGBO(6, 87, 96, 1),
         ),
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  const Text(
-                    'Arabic Font Size:',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15,
+          bottom: false,
+          child: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/background.png'),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const Text(
+                      'حجم الخط',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Color.fromRGBO(254, 249, 205, 1),
+                        fontFamily: 'quran',
+                        fontWeight: FontWeight.bold,
+                        shadows: [
+                          Shadow(
+                            offset: Offset(.5, .5),
+                            blurRadius: 1.0,
+                            color: Color.fromRGBO(6, 87, 96, 1),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  Slider(
-                    value: arabicFontSize,
-                    min: 20,
-                    max: 40,
-                    onChanged: (value) {
-                      setState(() {
-                        arabicFontSize = value;
-                      });
-                    },
-                  ),
-                  Text(
-                    "‏ ‏‏ ‏‏‏‏ ‏‏‏‏‏‏ ‏",
-                    style: TextStyle(
-                        fontFamily: 'quran', fontSize: arabicFontSize),
-                    textDirection: TextDirection.rtl,
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(top: 10, bottom: 10),
-                    child: Divider(),
-                  ),
-                  const Text(
-                    'Mushaf Mode Font Size:',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15,
+                    Slider(
+                      activeColor: const Color.fromRGBO(254, 249, 205, 1),
+                      value: arabicFontSize,
+                      min: 20,
+                      max: 40,
+                      onChanged: (value) {
+                        setState(() {
+                          arabicFontSize = value;
+                        });
+                        saveSettings();
+                      },
                     ),
-                  ),
-                  Slider(
-                    value: mushafFontSize,
-                    min: 20,
-                    max: 50,
-                    onChanged: (value) {
-                      setState(() {
-                        mushafFontSize = value;
-                      });
-                    },
-                  ),
-                  Text(
-                    "‏ ‏‏ ‏‏‏‏ ‏‏‏‏‏‏ ‏",
-                    style: TextStyle(
-                        fontFamily: 'quran', fontSize: mushafFontSize),
-                    textDirection: TextDirection.rtl,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              arabicFontSize = 28;
-                              mushafFontSize = 40;
-                            });
-                            saveSettings();
-                          },
-                          child: const Text('Reset')),
-                      ElevatedButton(
-                          onPressed: () {
-                            saveSettings();
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('Save')),
-                    ],
-                  ),
-                ],
+                    // Text(
+                    //   "‏ ‏‏ ‏‏‏‏ ‏‏‏‏‏‏ ‏",
+                    //   style: TextStyle(
+                    //       fontFamily: 'quran', fontSize: arabicFontSize),
+                    //   textDirection: TextDirection.rtl,
+                    // ),
+                    // const Padding(
+                    //   padding: EdgeInsets.only(top: 10, bottom: 10),
+                    //   child: Divider(),
+                    // ),
+                    // const Text(
+                    //   'Mushaf Mode Font Size:',
+                    //   style: TextStyle(
+                    //     fontWeight: FontWeight.bold,
+                    //     fontSize: 15,
+                    //   ),
+                    // ),
+                    // Slider(
+                    //   value: mushafFontSize,
+                    //   min: 20,
+                    //   max: 50,
+                    //   onChanged: (value) {
+                    //     setState(() {
+                    //       mushafFontSize = value;
+                    //     });
+                    //   },
+                    // ),
+                    // Text(
+                    //   "‏ ‏‏ ‏‏‏‏ ‏‏‏‏‏‏ ‏",
+                    //   style: TextStyle(
+                    //       fontFamily: 'quran', fontSize: mushafFontSize),
+                    //   textDirection: TextDirection.rtl,
+                    // ),
+                    // const SizedBox(
+                    //   height: 20,
+                    // ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                arabicFontSize = 28;
+                                mushafFontSize = 40;
+                              });
+                              saveSettings();
+                            },
+                            child: const Text('Reset')),
+                        ElevatedButton(
+                            onPressed: () {
+                              saveSettings();
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('Save')),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
